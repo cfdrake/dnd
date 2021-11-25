967b9b4d0c0fa7c95ea5e39ab518fccd24656e35
Colin Drake <github@accounts.colindrake.me>
2021-11-26 00:10:06 +0700

    Add default MIDI mapping for 16-knob controllers

--- alldrama.lua
+++ alldrama.lua
@@ -1,20 +1,14 @@
--- ~~ dnd ~~
--- drone & drama port
--- 3xsinfb & noise
--- thanks to: @bjc01
--- port by: @cfd90
-
-local ez = include "lib/ezscript"
+-- ~~ all drama ~~
+-- a drum machine for nerds
+-- by: @cfd90
+-- based on:
+--     dnd script
+-- and in turn:
+--       drone & drama
 
 engine.name = "DND"
 
-ez.pages = {
-  { name = "osc 1", e1 = "osc1Freq",   e2 = "osc1Fb",    e3 = "osc1Amp" },
-  { name = "osc 2", e1 = "osc2Freq",   e2 = "osc2Fb",    e3 = "osc2Amp" },
-  { name = "osc 3", e1 = "osc3Freq",   e2 = "osc3Fb",    e3 = "osc3Amp" },
-  { name = "osc *", e1 = "oscSlop",    e2 = nil,         e3 = nil },
-  { name = "noise", e1 = "filterFreq", e2 = "filterRes", e3 = "noiseAmp"}
-}
+local clk
 
 function init()
   params:add_separator("osc 1")
@@ -68,43 +62,97 @@ function init()
   
   params:bang()
   
-  m = midi.connect(1)
-  
-  m.event = function(data)
-    local msg = midi.to_msg(data)
-  
-    if msg.type ~= "cc" then
-      return
-    end
+  clk = clock.run(tick)
+end
+
+local pitches = {4, 8, 32, 34, 32, 34}
+local feedbacks = {3, 2, 1.9, 0, 1, 1}
+local rates = {1, 1, 1, 1, 1, 1}
+local idx = 1
+local steps = 6
+
+function p(n, v) pitches[n] = v end
+function f(n, v) feedbacks[n] = v end
+function r(n, v) rates[n] = v end
+function s(n) steps = n end
+
+function tick()
+  while true do
+    local rate = rates[idx]
+    clock.sync(1/rate)
     
-    local cc = msg.cc + 1
-    local val = msg.val
-    local pct = val / 127
+    params:set("osc1Amp", 0.05)
+    params:set("osc1Freq", pitches[idx])
+    params:set("osc1Fb", feedbacks[idx])
     
-    if cc == 1 then
-      params:set("osc1Freq", pct * 2000)
-    elseif cc == 2 then
-      params:set("osc1Fb", pct * 4)
-    elseif cc == 4 then
-      params:set("osc1Amp", pct * 1)
-    elseif cc == 5 then
-      params:set("osc2Freq", pct * 2000)
-    elseif cc == 6 then
-      params:set("osc2Fb", pct * 4)
-    elseif cc == 8 then
-      params:set("osc2Amp", pct * 1)
-    elseif cc == 9 then
-      params:set("osc3Freq", pct * 2000)
-    elseif cc == 10 then
-      params:set("osc3Fb", pct * 4)
-    elseif cc == 12 then
-      params:set("osc3Amp", pct * 1)
-    elseif cc == 13 then
-      params:set("filterFreq", pct * 10000)
-    elseif cc == 14 then
-      params:set("filterRes", pct * 3.9)
-    elseif cc == 16 then
-      params:set("noiseAmp", pct * 1)
+    redraw()
+    
+    idx = idx + 1
+    
+    if idx > steps then
+      idx = 1
     end
   end
+end
+
+function enc(n, d)
+  if n == 1 then
+    params:delta("clock_tempo", d)
+    redraw()
+  end
+end
+
+function redraw()
+  screen.clear()
+  
+  screen.move(0, 10)
+  screen.level(5)
+  screen.text("tempo: ")
+  screen.level(15)
+  screen.text(params:get("clock_tempo"))
+  screen.level(5)
+  screen.text(" | steps: ")
+  screen.level(15)
+  screen.text(steps)
+  
+  screen.move(0, 30)
+  screen.level(5)
+  
+  for i=1, #pitches do
+    screen.level(idx == i and 15 or 2)
+    screen.move((i * 18), 30)
+    screen.text(i)
+  end
+  
+  screen.move(0, 40)
+  screen.level(5)
+  screen.text("fq: ")
+  
+  for i=1, #pitches do
+    screen.level(idx == i and 15 or 5)
+    screen.move((i * 18), 40)
+    screen.text(pitches[i])
+  end
+  
+  screen.move(0, 50)
+  screen.level(5)
+  screen.text("fb: ")
+  
+  for i=1, #pitches do
+    screen.level(idx == i and 15 or 5)
+    screen.move((i * 18), 50)
+    screen.text(feedbacks[i])
+  end
+  
+  screen.move(0, 60)
+  screen.level(5)
+  screen.text("rt: ")
+  
+  for i=1, #pitches do
+    screen.level(idx == i and 15 or 5)
+    screen.move((i * 18), 60)
+    screen.text(rates[i])
+  end
+  
+  screen.update()
 end
